712100ace1ae1c7ae1af81f3837b9e419b4ce818
Isaiah Bullard <isaiah0812@yahoo.com>
2024-08-31 19:40:25 -0600

    menu functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,37 @@
+import { useState } from 'react'
 import './App.css'
 
+function BackButton({ onClick }) {
+  return (
+    <div>
+      {/* <img id="chevron" src="chevron-up-solid.svg" /> */}
+      <button id="back-button" onClick={onClick}>Back</button>
+      <br />
+    </div>
+  )
+}
+
+function Services() {
+  return <div>Services</div>
+}
+
+function Projects() {
+  return <div>Projects</div>
+}
+
+function Contact() {
+  return <div>Contact</div>
+}
+
 function App() {
+  const [selected, setSelected] = useState(undefined)
+
+  const menuItems = [
+    ['Services', <Services />],
+    ['Projects', <Projects />],
+    ['Contact', <Contact />]
+  ];
+
   return (
     <main>
       <h1 id="title">
@@ -10,12 +41,16 @@ function App() {
       </h1>
       <div id="content">
         <div id="menu">
-          <button className="section-button">Services</button>
-          <br />
-          <button className="section-button">Projects</button>
-          <br />
-          <button className="section-button">Contact</button>
+          {selected && <BackButton onClick={() => setSelected(undefined)} />}
+
+          {menuItems.map((item, ndx) =>
+            <>
+              {(!selected || selected[0] === item[0]) && (<button className="section-button" key={item[0]} onClick={() => setSelected(item)}>{item[0]}</button>)}
+              {((ndx !== menuItems.length - 1 && !selected) && <br />)}
+            </>
+          )}
         </div>
+        {selected && selected[1]}
       </div>
     </main>
   )
