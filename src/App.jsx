388efce34dbae3a5eaf3652b1fe6c84cb8c4df8e
Isaiah Bullard <isaiah0812@yahoo.com>
2024-08-31 21:06:34 -0600

    Contact section

--- src/App.jsx
+++ src/App.jsx
@@ -11,23 +11,51 @@ function BackButton({ onClick }) {
   )
 }
 
-function Services() {
-  return <div>Services</div>
-}
-
 function Projects() {
   return <div>Projects</div>
 }
 
 function Contact() {
-  return <div>Contact</div>
+  const [ callText, setCallText ] = useState('Call Me!')
+  const [ countDown, setCountDown ] = useState(5);
+  const [ rolling, setRolling ] = useState(false);
+
+  const ra = () => {
+    setCallText(`I'm so sorry...${countDown}`)
+
+    const raTimer = setInterval(() => {
+      setCountDown((prev) => {
+        if (prev === 0) {
+          clearInterval(raTimer);
+          setRolling(true);
+          setCallText("I can't have random people calling me.")
+          return 0;
+        } else {
+          setCallText(`I'm so sorry...${prev - 1}`)
+          return prev - 1;
+        }
+      })
+    }, 1000)
+
+
+    return () => window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ').focus();
+  }
+  return (
+    <div>
+      Email: <a href="mailto:zae@zaemakeswebsites.com">zae@zaemakeswebsites.com</a>
+      <br />
+      <button onClick={ra}>{callText}</button>
+      {rolling && (
+        <iframe width="420" height="345" src="http://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allowfullscreen></iframe>
+      )}
+    </div>
+  )
 }
 
 function App() {
   const [selected, setSelected] = useState(undefined)
 
   const menuItems = [
-    ['Services', <Services />],
     ['Projects', <Projects />],
     ['Contact', <Contact />]
   ];
